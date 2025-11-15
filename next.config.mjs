ff9c46c9cbfc1e9c28fca15e8f8985bb488a7b3b
jay19patel <justjayy19@gmail.com>
2025-11-15 12:07:10 +0530

    no

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    domains: ["via.placeholder.com", "images.unsplash.com", "randomuser.me"],
+  },
 };
 
 export default nextConfig;
