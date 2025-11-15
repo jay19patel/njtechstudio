61d4dbace12f7d94f6459edf1319072f9de98969
jay19patel <justjayy19@gmail.com>
2025-11-15 15:27:18 +0530

    init

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,6 @@ import HeroSection from "./components/HeroSection";
 import TechSkillSection from "./components/TechSkillSection";
 import SolutionsSection from "./components/SolutionsSection";
 import FAQSection from "./components/faqSection";
-import CTASection from "./components/CTASection";
-import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
@@ -11,8 +9,6 @@ export default function Home() {
       <HeroSection />
       <SolutionsSection />
       <TechSkillSection />
-      <AboutSection />
-      <CTASection />
       <FAQSection />
     </div>
   );
